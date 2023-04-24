35d060fd94d4b979f37650e7d19c9d6cdfd6d315
izharj <eezhar@gmail.com>
2023-04-25 02:01:30 +0800

    create Post Dto

---
+++ server/src/main/java/izhar/tutorlah/server/dto/PostDto.java
@@ -0,0 +1,14 @@
+package izhar.tutorlah.server.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class PostDto {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime creationDateTime;
+
+}
---
+++ server/src/main/java/izhar/tutorlah/server/service/PostService.java
@@ -0,0 +1,6 @@
+package izhar.tutorlah.server.service;
+
+public interface PostService {
+
+
+}
