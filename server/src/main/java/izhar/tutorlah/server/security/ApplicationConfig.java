7892108bd7eb7f5dabf2d61219a9f6b84c2a64f4
izharj <eezhar@gmail.com>
2023-04-25 00:23:17 +0800

    edit password encoder as bean

--- server/src/main/java/izhar/tutorlah/server/security/ApplicationConfig.java
+++ server/src/main/java/izhar/tutorlah/server/security/ApplicationConfig.java
@@ -31,7 +31,8 @@ public class ApplicationConfig {
         return authProvider;
     }
 
-    private PasswordEncoder passwordEncoder() {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
