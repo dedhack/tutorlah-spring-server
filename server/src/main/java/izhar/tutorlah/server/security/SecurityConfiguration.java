dad0ab4d5321deb0ff54463761fa8329a2179459
izharj <eezhar@gmail.com>
2023-04-24 21:35:44 +0800

    updated POM dependencies

--- server/src/main/java/izhar/tutorlah/server/security/SecurityConfiguration.java
+++ server/src/main/java/izhar/tutorlah/server/security/SecurityConfiguration.java
@@ -1,14 +1,26 @@
 package izhar.tutorlah.server.security;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private AuthenticationProvider authenticationProvider;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
